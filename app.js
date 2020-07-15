c681a24da89fc6396a17a63510b7d159ed75aa56
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-07-15 20:55:07 +0530

    database connection configuration

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const { Client } = require("pg");
 const morgan = require("morgan");
 const chalk = require("chalk");
 
@@ -7,6 +9,23 @@ const productRoutes = require("./routes/products");
 
 const app = express();
 
+// postgres pool config
+const client = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Allshop",
+  password: process.env.DATABASE_PASSWORD,
+  port: 5432,
+});
+
+// connecting to database
+client.connect(err =>
+  err
+    ? console.log(chalk.red(err))
+    : console.log(chalk.blue("Database connected"))
+);
+
+// Logger
 app.use(morgan("dev"));
 
 app.get("/", function (req, res) {
@@ -16,11 +35,10 @@ app.get("/", function (req, res) {
 // path to products.js
 app.use("/products", productRoutes);
 
+// Error handler
 app.use(function (err, req, res, next) {
   console.log(chalk.red(err.stack));
   res.status(400).json({ success: false, error: err.message });
 });
 
-app.listen(8000, () =>
-  console.log(chalk.yellow("Server started on port 8000"))
-);
+app.listen(8000, () => console.log(chalk.blue("Server started on port 8000")));
