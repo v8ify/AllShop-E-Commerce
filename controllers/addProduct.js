7a215ca8e104604454643007bd0cdaab461cfaa4
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-07-16 11:27:47 +0530

    Added sql query to addProduct

--- controllers/addProduct.js
+++ controllers/addProduct.js
@@ -1,3 +1,25 @@
+const client = require("../db/index");
+
 exports.addProduct = function (req, res, next) {
-  console.log(req.body);
+  const {
+    seller_id,
+    name,
+    quantity_left,
+    description,
+    price,
+    discount,
+  } = req.body;
+
+  const queryText =
+    "INSERT INTO items_for_sale(seller_id, name, quantity_left, description, price, discount) VALUES($1, $2, $3, $4, $5, $6) RETURNING *";
+
+  const values = [seller_id, name, quantity_left, description, price, discount];
+
+  client.query(queryText, values, function (err, result) {
+    if (err) {
+      return next(err);
+    }
+
+    res.status(200).json({ success: true, data: result.rows });
+  });
 };
