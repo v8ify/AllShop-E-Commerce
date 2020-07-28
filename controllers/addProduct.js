06cb9a811aaec9314df3b437b70ab7e2ade8168e
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-07-28 21:32:42 +0530

    refactored to implement sequelize

--- controllers/addProduct.js
+++ controllers/addProduct.js
@@ -1,6 +1,6 @@
-const client = require("../db/index");
+const ItemToSell = require("../models/ItemToSell");
 
-exports.addProduct = function (req, res, next) {
+exports.addProduct = async function (req, res, next) {
   const {
     seller_id,
     name,
@@ -10,16 +10,20 @@ exports.addProduct = function (req, res, next) {
     discount,
   } = req.body;
 
-  const queryText =
-    "INSERT INTO items_for_sale(seller_id, name, quantity_left, description, price, discount) VALUES($1, $2, $3, $4, $5, $6) RETURNING *";
-
-  const values = [seller_id, name, quantity_left, description, price, discount];
-
-  client.query(queryText, values, function (err, result) {
-    if (err) {
-      return next(err);
-    }
-
-    res.status(200).json({ success: true, data: result.rows });
+  let item = ItemToSell.build({
+    seller_id,
+    name,
+    quantity_left,
+    description,
+    price,
+    discount,
   });
+
+  try {
+    item = await item.save();
+  } catch (error) {
+    next(error);
+  }
+
+  res.status(200).json({ success: true, data: item });
 };
