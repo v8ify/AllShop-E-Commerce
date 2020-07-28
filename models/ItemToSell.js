504fc016267f58600b6fe8769f7d8ef425671763
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-07-28 21:00:29 +0530

    completed itemtosell model

--- models/ItemToSell.js
+++ models/ItemToSell.js
@@ -1,11 +1,53 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../db/index");
 
-const ItemToSell = sequelize.define("ItemToSell", {
-  seller_id: {
-    type: DataTypes.INTEGER,
-    validate: {
-      notNull: { msg: "seller_id cannot be null" },
+const ItemToSell = sequelize.define(
+  "ItemToSell",
+  {
+    seller_id: {
+      type: DataTypes.INTEGER,
+      validate: {
+        notNull: { msg: "seller_id cannot be null" },
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    quantity_left: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: { args: 1, msg: "quantity should be more than or equal to 1" },
+        isInt: { msg: "quantity must be an interger" },
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      validate: {
+        notNull: {
+          msg: "price cannot be null",
+        },
+        isInt: {
+          msg: "price must be int",
+        },
+      },
+    },
+    discount: {
+      type: DataTypes.INTEGER,
+      validate: {
+        notNull: {
+          msg: "price cannot be null",
+        },
+        isInt: {
+          msg: "price must be int",
+        },
+      },
     },
   },
-});
+
+  {
+    tableName: "items_for_sale",
+  }
+);
